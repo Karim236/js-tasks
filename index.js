1c69986b427caaa812479385af21e2ce309c72d5
unknown <karimboy2306@gmail.com>
2022-06-11 23:01:31 +0300

    pyFunc

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ const mult = (a, b) =>{
     return c;
 }
 
+const pyFunc = (number, minimum) => {
+   
+    if (number % 3 == 0) {
+        let min = Math.min(minimum, number)
+        console.log(min)
+    } else {
+    console.log("NAN")
+    }
+
+}
+
 helloWorld()
 summary(50, 6)
 mult(9, 4)
+pyFunc(42, 30001)
